2ee171b905599bab01d6edb29c4da92c94ada9d7
vihith07 <arekatlavihith@gmail.com>
2024-05-28 15:52:15 +0530

    initial version with lock on obj monitor

---
+++ src/main/java/executor/MyBlockingQueue.java
@@ -0,0 +1,42 @@
+package executor;
+
+import java.util.LinkedList;
+
+public class MyBlockingQueue<T> {
+    private final LinkedList<T> queue;
+    private int capacity=0;
+
+    public MyBlockingQueue(int capacity){
+        this.queue=new LinkedList<>();
+        this.capacity=capacity;
+    }
+
+    synchronized public T pop() throws Exception {
+        if(capacity<=0){throw new Exception("queue capacity - "+capacity+"; pop error !!!");}
+        while(queue.isEmpty()){
+            this.wait();
+        }
+        if(queue.size()==capacity){
+            this.notifyAll();
+        }
+        return queue.poll();
+    }
+
+    synchronized public void push(T obj) throws Exception {
+        if(capacity<=0){throw new Exception("queue capacity - "+capacity+"; push error !!!");}
+        if(queue.isEmpty()){
+            queue.add(obj);
+            this.notifyAll();
+        }
+        else{
+            while(queue.size()==capacity){
+                this.wait();
+            }
+            queue.add(obj);
+        }
+    }
+
+    synchronized  public LinkedList<T> getList(){
+        return this.queue;
+    }
+}
---
+++ src/main/java/executor/MyExecutorService.java
@@ -0,0 +1,8 @@
+package executor;
+
+import java.util.LinkedList;
+
+public class MyExecutorService {
+
+
+}
